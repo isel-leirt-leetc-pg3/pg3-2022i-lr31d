427923a952010277791fef77c12d1f3a75780101
Jorge Martins <jmartins@isel.pt>
2021-10-01 12:26:59 +0100

    first try

---
+++ app/src/main/java/lr31d/Main.java
@@ -0,0 +1,7 @@
+package lr31d;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ app/src/main/java/utils/X.java
@@ -0,0 +1,8 @@
+package utils;
+import lr31d.*;
+
+public class X {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
